3da3bca948615815dd7e89382e42bab3bd5c9e50
Plutorange <114486060+Plutorange@users.noreply.github.com>
2023-01-28 13:37:30 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import os
 
 import pygame
 
-pygame.init()
-
 
 def load_image(name, color_key=None):
     fullname = os.path.join('bil', name)
@@ -22,9 +20,25 @@ def load_image(name, color_key=None):
     return image
 
 
+class GameInfo:
+    pygame.init()
+    pygame.display.set_caption('Шарики')
+    size = width, height = 1000, 800
+    board_width, board_height = 900, 480
+    screen = pygame.display.set_mode(size)
+    all_sprites = pygame.sprite.Group()
+    out_sprites = pygame.sprite.Group()
+    corners = pygame.sprite.Group()
+    corner_image = load_image('corner.png', -1)
+    corner_image = pygame.transform.scale(corner_image, (45, 45))
+    winner = ''
+
+
 class Ball(pygame.sprite.Sprite):
-    def __init__(self, pos, num):
-        super().__init__(all_sprites)
+    def __init__(self, pos, num, sprlist):
+        super().__init__(sprlist)
+        GameInfo.all_sprites.add(self)
+        width, height = GameInfo.width, GameInfo.height
         ball_image = load_image(f'ball{num + 1}.png', -1)
         ball_image = pygame.transform.scale(ball_image, (30, 30))
         self.image = ball_image
@@ -134,7 +148,7 @@ class Ball(pygame.sprite.Sprite):
             self.speed_x *= -1
         if self.rect.y + self.speed_y - 30 < 160:
             self.speed_y *= -1
-        for i in all_sprites:
+        for i in GameInfo.all_sprites:
             self.rect = self.rect.move(self.dist_x + self.speed_x - self.rect.x,
                                        self.dist_y + self.speed_y - self.rect.y)
             if self != i and pygame.sprite.collide_mask(self, i):
@@ -148,146 +162,137 @@ class Ball(pygame.sprite.Sprite):
         if abs(self.speed_x) < 0.01 and abs(self.speed_y) < 0.01:
             self.speed_x = 0
             self.speed_y = 0
-        for i in all_sprites:
+        for i in GameInfo.all_sprites:
             if pygame.sprite.collide_mask(self, i) and self != i:
                 self.dist_x -= self.speed_x
                 self.dist_y -= self.speed_y
                 self.rect = self.rect.move(self.dist_x - self.rect.x, self.dist_y - self.rect.y)
         self.speed_x /= 1.005
         self.speed_y /= 1.005
-        for i in corners:
+        for i in GameInfo.corners:
             if pygame.sprite.collide_mask(self, i):
-                all_sprites.remove(self)
-                if self != sp_ball:
-                    out_sprites.add(self)
+                GameInfo.all_sprites.remove(self)
+                if self != Game.sp_ball:
+                    GameInfo.out_sprites.add(self)
                 break
 
 
 class Corner(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__(corners)
-        self.image = corner_image
+    def __init__(self, sprlist):
+        super().__init__(sprlist)
+        GameInfo.corners.add(self)
+        self.image = GameInfo.corner_image
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        if len(corners) == 1:
+        if len(GameInfo.corners) == 1:
             self.rect.x = 50
             self.rect.y = 160
-        elif len(corners) == 2:
+        elif len(GameInfo.corners) == 2:
             self.rect.x = 50
             self.rect.y = 595
-        elif len(corners) == 3:
+        elif len(GameInfo.corners) == 3:
             self.rect.x = 905
             self.rect.y = 160
-        elif len(corners) == 4:
+        elif len(GameInfo.corners) == 4:
             self.rect.x = 905
             self.rect.y = 595
-        elif len(corners) == 5:
+        elif len(GameInfo.corners) == 5:
             self.rect.x = 485
             self.rect.y = 160
-        elif len(corners) == 6:
+        elif len(GameInfo.corners) == 6:
             self.rect.x = 485
             self.rect.y = 595
 
 
-pygame.display.set_caption('Шарики')
-size = width, height = 1000, 800
-board_width, board_height = 900, 480
-screen = pygame.display.set_mode(size)
-all_sprites = pygame.sprite.Group()
-out_sprites = pygame.sprite.Group()
-corners = pygame.sprite.Group()
-corner_image = load_image('corner.png', -1)
-corner_image = pygame.transform.scale(corner_image, (45, 45))
-sp_ball = Ball((500, 400), 15)
-winner = ''
+class Game:
+    sp_ball = Ball((500, 400), 15, GameInfo.all_sprites)
 
-
-def pygame_start():
-    for i in range(6):
-        Corner()
-    for i in range(15):
-        Ball((500, 400), i)
-    global sp_ball
-    global winner
-    name1, name2 = Login(), Login_2()
-    username, username2 = name1.get_nickname(), name2.get_nickname()
-    scream_image = pygame.transform.scale(load_image('scream.jpeg'), (1000, 800))
-    bait_image = pygame.transform.scale(load_image('butto.jpeg'), (25, 25))
-    turn = 0
-    text1 = pygame.font.Font(None, 40).render(username, True, (36, 9, 53))
-    text2 = pygame.font.Font(None, 40).render(username2, True, (36, 9, 53))
-    p1_balls = [None, []]
-    p2_balls = [None, []]
-    clock = pygame.time.Clock()
-    scr = False
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if 50 <= event.pos[0] < 950 and 160 <= event.pos[1] < 640:
-                    if sp_ball not in all_sprites:
-                        sp_ball = Ball(event.pos, 15)
+    @staticmethod
+    def pygame_start():
+        for i in range(6):
+            Corner(GameInfo.corners)
+        for i in range(15):
+            Ball((500, 400), i, GameInfo.all_sprites)
+        scream_image = pygame.transform.scale(load_image('scream.jpeg'), (1000, 800))
+        bait_image = pygame.transform.scale(load_image('butto.jpeg'), (25, 25))
+        turn = 0
+        text1 = pygame.font.Font(None, 40).render('username', True, (36, 9, 53))
+        text2 = pygame.font.Font(None, 40).render('username2', True, (36, 9, 53))
+        p1_balls = [None, []]
+        p2_balls = [None, []]
+        clock = pygame.time.Clock()
+        scr = False
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if 50 <= event.pos[0] < 950 and 160 <= event.pos[1] < 640:
+                        if Game.sp_ball not in GameInfo.all_sprites:
+                            GameInfo.sp_ball = Ball(event.pos, 15, GameInfo.all_sprites)
+                        else:
+                            if Game.sp_ball.speed_x == 0 == Game.sp_ball.speed_y:
+                                Game.sp_ball.speed_x = (Game.sp_ball.rect.x + 15 - event.pos[0]) / 50
+                                Game.sp_ball.speed_y = (Game.sp_ball.rect.y + 15 - event.pos[1]) / 50
+                    elif 975 <= event.pos[0] and event.pos[1] <= 25:
+                        scr = True
+            if len(GameInfo.out_sprites) == 1:
+                a = [i for i in GameInfo.out_sprites][0]
+                if a.num < 7:
+                    if turn == 0:
+                        p1_balls = ['solid', [a.num]]
                     else:
-                        if sp_ball.speed_x == 0 == sp_ball.speed_y:
-                            sp_ball.speed_x = (sp_ball.rect.x + 15 - event.pos[0]) / 50
-                            sp_ball.speed_y = (sp_ball.rect.y + 15 - event.pos[1]) / 50
-                elif 975 <= event.pos[0] and event.pos[1] <= 25:
-                    scr = True
-        if len(out_sprites) == 1:
-            a = [i for i in out_sprites][0]
-            if a.num < 7:
-                if turn == 0:
-                    p1_balls = ['solid', [a.num]]
+                        p2_balls = ['solid', [a.num]]
+                elif a.num > 8:
+                    if turn == 0:
+                        p1_balls = ['stripe', [a.num]]
+                    else:
+                        p2_balls = ['stripe', [a.num]]
+            elif len(GameInfo.out_sprites) > 1:
+                a = [i for i in GameInfo.out_sprites][-1]
+                if (p1_balls[0] == 'solid' and a.num < 7) or (p1_balls[0] == 'stripe' and a.num > 8):
+                    p1_balls[1].append(a.num)
                 else:
-                    p2_balls = ['solid', [a.num]]
-            elif a.num > 8:
-                if turn == 0:
-                    p1_balls = ['stripe', [a.num]]
+                    p2_balls[1].append(a.num)
+            if Game.sp_ball.speed_x == 0 or 0 == Game.sp_ball.speed_y:
+                turn = (turn + 1) % 2
+            GameInfo.screen.fill((40, 120, 80))
+            pygame.draw.rect(GameInfo.screen, (50, 200, 50), (80, 190, 840, 420))
+            pygame.draw.rect(GameInfo.screen, (150, 75, 0), (50, 160, 900, 30))
+            pygame.draw.rect(GameInfo.screen, (150, 75, 0), (50, 160, 30, 480))
+            pygame.draw.rect(GameInfo.screen, (150, 75, 0), (50, 610, 900, 30))
+            pygame.draw.rect(GameInfo.screen, (150, 75, 0), (920, 160, 30, 480))
+            pygame.draw.circle(GameInfo.screen, (0, 0, 0), (500, 400), 10)
+            pygame.draw.rect(GameInfo.screen, (220, 220, 220), (30, 30, 300, 100), border_radius=100)
+            pygame.draw.rect(GameInfo.screen, (0, 0, 0), (30, 30, 300, 100), 5, border_radius=100)
+            pygame.draw.rect(GameInfo.screen, (220, 220, 220), (670, 30, 300, 100),
+                             border_radius=100)
+            pygame.draw.rect(GameInfo.screen, (0, 0, 0), (670, 30, 300, 100), 5, border_radius=100)
+            GameInfo.screen.blit(text1, (70, 40))
+            GameInfo.screen.blit(text2, (710, 40))
+            GameInfo.screen.blit(bait_image, (975, 0))
+            if scr:
+                GameInfo.screen.blit(scream_image, (0, 0))
+                pygame.display.flip()
+                pygame.time.delay(5000)
+                scr = False
+            else:
+                GameInfo.corners.draw(GameInfo.screen)
+                GameInfo.all_sprites.draw(GameInfo.screen)
+                GameInfo.all_sprites.update()
+                pygame.display.flip()
+                clock.tick(150)
+            if len(p1_balls[1]) == 8 or len(p2_balls[1]) == 8:
+                GameInfo.screen.fill((0, 0, 0))
+                pygame.display.flip()
+                pygame.time.delay(1000)
+                if len(p1_balls[1]) == 8:
+                    Game.winner = 'username'
                 else:
-                    p2_balls = ['stripe', [a.num]]
-        elif len(out_sprites) > 1:
-            a = [i for i in out_sprites][-1]
-            if (p1_balls[0] == 'solid' and a.num < 7) or (p1_balls[0] == 'stripe' and a.num > 8):
-                p1_balls[1].append(a.num)
-            else:
-                p2_balls[1].append(a.num)
-        if sp_ball.speed_x == 0 == sp_ball.speed_y:
-            turn = (turn + 1) % 2
-        screen.fill((40, 120, 80))
-        pygame.draw.rect(screen, (50, 200, 50), (80, 190, 840, 420))
-        pygame.draw.rect(screen, (150, 75, 0), (50, 160, 900, 30))
-        pygame.draw.rect(screen, (150, 75, 0), (50, 160, 30, 480))
-        pygame.draw.rect(screen, (150, 75, 0), (50, 610, 900, 30))
-        pygame.draw.rect(screen, (150, 75, 0), (920, 160, 30, 480))
-        pygame.draw.circle(screen, (0, 0, 0), (500, 400), 10)
-        pygame.draw.rect(screen, (220, 220, 220), (30, 30, 300, 100), border_radius=100)
-        pygame.draw.rect(screen, (0, 0, 0), (30, 30, 300, 100), 5, border_radius=100)
-        pygame.draw.rect(screen, (220, 220, 220), (670, 30, 300, 100), border_radius=100)
-        pygame.draw.rect(screen, (0, 0, 0), (670, 30, 300, 100), 5, border_radius=100)
-        screen.blit(text1, (70, 40))
-        screen.blit(text2, (710, 40))
-        screen.blit(bait_image, (975, 0))
-        if scr:
-            screen.blit(scream_image, (0, 0))
-            pygame.display.flip()
-            pygame.time.delay(5000)
-            scr = False
-        else:
-            corners.draw(screen)
-            all_sprites.draw(screen)
-            all_sprites.update()
-            pygame.display.flip()
-            clock.tick(150)
-        if len(p1_balls[1]) == 8 or len(p2_balls[1]) == 8:
-            screen.fill((0, 0, 0))
-            pygame.display.flip()
-            pygame.time.delay(1000)
-            if len(p1_balls[1]) == 8:
-                winner = username
-            else:
-                winner = username2
-            pygame.quit()
+                    Game.winner = 'username2'
+                pygame.quit()
+                return
 
 
-pygame_start()
+Game.pygame_start()
